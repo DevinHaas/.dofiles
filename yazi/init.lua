439ef47bf575543af6bebbbbf34f1c731c5ff9d5
Devin Halser <devin.hasler@bluewin.ch>
2026-03-09 18:14:25 +0100

    added zathura for tui pdf views

---
+++ yazi/init.lua
@@ -0,0 +1,8 @@
+require("save-clipboard-to-file"):setup({
+	-- Position of input file name or path dialog
+	input_position = { "center", w = 70 },
+	-- Position of overwrite confirm dialog
+	overwrite_confirm_position = { "center", w = 70, h = 10 },
+	-- Hide notify
+	hide_notify = false,
+})
